646110f1d976a7e31d0e5e269488e635ac778195
jnu6103 <jinu6103@naver.com>
2023-04-07 00:16:42 +0900

    update README.md
    update simple_calculator.py

--- simple_calculator.py
+++ simple_calculator.py
@@ -10,9 +10,9 @@ while True:
     if op == "end":
         break   # "반복을 종료"
     elif op == "+":
-        num1, num2, ret = arithmetic_ops(add) # 정의된 함수 사용
+        num1, num2, ret = arithmetic_ops(add) 
     elif op == "-":
-        num1. num2, ret = arithmetic_ops(sub) # 익명함수(lambda) 사용
+        num1. num2, ret = arithmetic_ops(sub)
     elif op == "-":
         num1, num2, ret = arithmetic_ops(lambda x,y:x*y)
     elif op == "/": 
@@ -21,7 +21,7 @@ while True:
         num1, num2, ret = arithmetic_ops(lambda x,y:x%y)
     else:
         print("Invalid operation")
-        continue # Invalid operation이므로 연산결과를 출력하지 않고 "넘어간다".
-    print(f"{num1}{op}{num2} = {ret}")  # 연산 결과를 출력
+        continue 
+    print(f"{num1}{op}{num2} = {ret}")  
 
 print("Exit program")
